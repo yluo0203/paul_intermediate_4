5c21ec7463fb54dbf0608e360f872a03c0f45b6c
yluo0203 <yluo0203@gmail.com>
2017-01-27 16:35:03 -0800

    Initial Commit

---
+++ paul_intermediate_4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  paul_intermediate_4
+//
+//  Created by Tse Jen lu on 27/01/2017.
+//  Copyright © 2017 Tse Jen Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
